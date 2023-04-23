99d1d44cfad51cd1a317eb417a886f39ef3a1623
athulAnilkumarAppu <athulAnilkumarAppu@gmail.com>
2023-04-23 12:41:44 +0530

    portchange

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const pdfTemplate = require('./documents')
 
 const app = express()
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 
 app.use(cors())
 app.use(bodyParser.urlencoded({extended:true}))
