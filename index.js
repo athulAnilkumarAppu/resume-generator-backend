ff56722c91f8aa7a4d4ee1c3f32df4883416e4e7
athulAnilkumarAppu <athulAnilkumarAppu@gmail.com>
2023-04-23 12:45:26 +0530

    error msg returned

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json())
 app.post('/create-pdf', (req, res)=> {
     pdf.create(pdfTemplate(req.body), {}).toFile('resume.pdf', (err)=> {
         if(err){
-             res.send(false)
+             res.send({status: false, errorMessage: err})
         }else{
             res.send(true)
         }
